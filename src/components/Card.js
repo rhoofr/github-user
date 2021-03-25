58c8f4aa20d52c406bee5d738272f6d866746935
Richard Hoofring <rhoofr@yahoo.com>
2021-03-25 10:16:29 -0400

    setup context with mock data and add user info and card component details

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
+
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser } = useContext(GithubContext);
+  const {
+    avatar_url,
+    html_url,
+    name,
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username
+  } = githubUser;
+
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>@{twitter_username || 'non-tweeter'}</p>
+        </div>
+        <a href={html_url}>follow</a>
+      </header>
+      <p className='bio'>{bio || 'no bio'}</p>
+      <div className='links'>
+        <p>
+          <MdBusiness />
+          {company || 'no company'}
+        </p>
+        <p>
+          <MdLocationOn />
+          {location || 'no location'}
+        </p>
+        {blog ? (
+          <a href={`https://${blog}`}>
+            <MdLink />
+            {blog}
+          </a>
+        ) : (
+          <p>
+            <MdLink />
+            {'no blog url'}
+          </p>
+        )}
+      </div>
+    </Wrapper>
+  );
 };
+
 const Wrapper = styled.article`
   background: var(--clr-white);
   padding: 1.5rem 2rem;
--- src/context/context.js
+++ src/context/context.js
@@ -10,8 +10,14 @@ const GithubContext = React.createContext();
 
 // Provider, Consumer
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+
   return (
-    <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+      {children}
+    </GithubContext.Provider>
   );
 };
 
