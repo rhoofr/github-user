a0a47f5b3633d54f8e25980fd0877fc2a85fbd9a
Richard Hoofring <rhoofr@yahoo.com>
2021-03-25 20:10:46 -0400

    build 4 charts for repos

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,44 @@
+// Include react
 import React from 'react';
+// Include the react-fusioncharts component
+import ReactFC from 'react-fusioncharts';
+// Include the fusioncharts library
+import FusionCharts from 'fusioncharts';
+// Include the chart type
+import Chart from 'fusioncharts/fusioncharts.charts';
+// Include the theme as fusion
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
-const Pie3D = () => {
-  return <div>chart</div>;
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const Pie3D = ({ data }) => {
+  const chartConfigs = {
+    type: 'pie3d', // The chart type
+    width: '100%', // Width of the chart
+    height: '400', // Height of the chart
+    dataFormat: 'json', // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        //Set the chart caption
+        caption: 'Languages',
+        pieRadius: '35%',
+        //Set the chart subcaption
+        subCaption: 'In MMbbl = One Million barrels',
+        //Set the x-axis name
+        xAxisName: 'Country',
+        //Set the y-axis name
+        yAxisName: 'Reserves (MMbbl)',
+        //Set the theme for your chart
+        theme: 'fusion'
+      },
+      // Chart Data
+      data
+    }
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
 
 export default Pie3D;
